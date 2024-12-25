04f24588f932420f7b3486dcdd309797322a572b
lassmed <mohamed.lasswed2001@gmail.com>
2024-12-25 12:14:02 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "76b3e3d7-28bd-4ac7-a064-223227940560"
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "aksdemo"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
